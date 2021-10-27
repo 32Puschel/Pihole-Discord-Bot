e9cc77b4228e418e6871f140898ecc2926a0ab42
Puschel <68943564+32Puschel@users.noreply.github.com>
2021-10-27 22:22:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,22 +14,32 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == 'pi/on': #turns the blocking on
-        on = requests.get('http://192.168.188.99/admin/api.php?enable&auth=APITOKEN')
+        on = requests.get('http://IpAdressofyourPI/admin/api.php?enable&auth=APIKEY')
         await message.channel.send(on.text)
     if message.content == 'pi/off': #turns the blocking off
-        off = requests.get('http://192.168.188.99/admin/api.php?disable&auth=APITOKEN')
+        off = requests.get('http://IpAdressofyourPI/admin/api.php?disable&auth=APIKEY')
         await message.channel.send(off.text)
     if message.content == 'pi/ver': #not the actual Version, only the API Version
-        ver = requests.get('http://192.168.188.99/admin/api.php?version&auth=APITOKEN')
+        ver = requests.get('http://IpAdressofyourPI/admin/api.php?version&auth=APIKEY')
         await message.channel.send(ver.text)
     if message.content == 'pi/sum': #shows a summary of random statistics
-        sum = requests.get('http://192.168.188.99/admin/api.php?summary&auth=APITOKEN')
+        sum = requests.get('http://IpAdressofyourPI/admin/api.php?summary&auth=APIKEY')
         # pformat from pprint module, will convert json into a nicer looking string thanks random from the Internet
         await message.channel.send (pformat(sum.json()))
     if message.content == 'pi/top': #shows the top 10 domains
-        top = requests.get("http://192.168.188.99/admin/api.php?topItems&auth=APITOKEN")
+        top = requests.get("http://IpAdressofyourPI/admin/api.php?topItems&auth=APIKEY")
         await message.channel.send (pformat(top.json()))
-       
+    if message.content == 'pi/qt': #shows the query types
+        QT = requests.get('http://IpAdressofyourPI/admin/api.php?getQueryTypes&auth=APIKEY')
+        await message.channel.send (pformat(QT.json()))
+    if message.content == 'pi/qs': #shows the top Query Sources
+        QS = requests.get('http://IpAdressofyourPI/admin/api.php?getQuerySources&auth=APIKEY')
+        await message.channel.send (pformat(QS.json()))
+    if message.content == 'pi/des': #shows the top Query Sources This does not work properly and I have no Idea why
+        Des = requests.get('http://IpAdressofyourPI/admin/api.php?getForwardDestinations&auth=APIKEY')
+        await message.channel.send (pformat(Des.json()))
+
+        
         
 
-client.run('<yourToken>')
+client.run('Your-Bot-Token')
